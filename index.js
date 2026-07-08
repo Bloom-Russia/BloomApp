7d95f81c7fc32e73d7776a9efff672d38b2b21cc
Aleksey <ostrikov.as@incloud.com>
2026-07-08 23:05:15 +0200

    working

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/app/App';
 
-// if (__DEV__) {
-//   import('reactotron-react-native')
-//     .then(({ default: Reactotron }) => {
-//       Reactotron.configure({
-//         host: '192.168.1.54',
-//         name: 'Bloom',
-//         port: 9090,
-//       })
-//         .useReactNative({
-//           networking: {
-//             ignoreUrls: /symbolicate|logs/,
-//           },
-//           overlay: false,
-//         })
-//         .connect();
-//     })
-//     .catch((error) => {
-//       console.warn('Reactotron not available:', error.message);
-//     });
-// }
+if (__DEV__) {
+  import('reactotron-react-native')
+    .then(({ default: Reactotron }) => {
+      Reactotron.configure({
+        host: '192.168.1.54',
+        name: 'Bloom',
+        port: 9090,
+      })
+        .useReactNative({
+          networking: {
+            ignoreUrls: /symbolicate|logs/,
+          },
+          overlay: false,
+        })
+        .connect();
+    })
+    .catch((error) => {
+      console.warn('Reactotron not available:', error.message);
+    });
+}
 
 AppRegistry.registerComponent(appName, () => App);
